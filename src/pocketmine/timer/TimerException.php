6851b6b00e08179dd4f5cd816a524ad7db7c3894
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2022-08-12 17:33:09 +0700

    Update TimerException.php

--- src/pocketmine/timer/TimerException.php
+++ src/pocketmine/timer/TimerException.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace src\pocketmine\timer;
+
+final class TimerException extends \Exception{
+     
+     private string $message;
+    
+     public function __construct(string $message){
+      $this->message = $message;
+  }
+  
+     public function __tostring() : string{
+      return "Error: " . $this->message . " in line " . $this->line . " and in the file " . $this->file;
+  }
+}
